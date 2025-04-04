ce8a5c6520fdf4fc4e3c45d8d0ee453641fc7e68
sandhyachan <sandhyachan6@gmail.com>
2025-04-04 17:45:57 +0530

    add forgot and update password API

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -128,6 +128,83 @@ const updateUser = async (req, res) => {
     }
 }
 
+const updatePassword = async ( req, res ) => {
+    const {username} = req.user
+    const { currentPassword, newPassword} = req.body
 
+    try {
+        const matchingUser = await UserModel.findOne({username})
+
+        if (!matchingUser) {
+            return res.status(404).json({
+                message: "User not found. Please log in."
+            })
+        }
+
+        const passwordMatches = await bcrypt.compare(currentPassword, matchingUser.password)
+    
+        if ( !passwordMatches ){
+            return res.status(400).json({
+                message: "Your current password is incorrect. Please enter the correct password or use the reset password option."
+            })
+        }
+        if ( newPassword.length < 8 ){
+            return res.status(400).json("New password must be at least 8 characters long. ")
+        }
+        
+        const hashedPassword = await bcrypt.hash(newPassword,10)
+        matchingUser.password = hashedPassword
+        await matchingUser.save()
+    
+        return res.status(200).json({
+            message: "Password updated successfully."
+        })
+    } catch (error) {
+        console.error(error);  // Log the error for debugging
+        return res.status(500).json({
+            message: "An error occurred while updating the password. Please try again later."
+        })
+    }
+}
+
+const forgotPassword = async (req, res) => {
+    const { email } = req.body
+
+    try {
+        const matchingUser = await UserModel.findOne({email})
+        if(!matchingUser){
+            return res.status(404).json({ message: 'Account not found!' })
+        }
+    
+        const OTP = generateOtp()
+        const resetLink = `https://localhost:3000/reset-password`
+    
+        matchingUser.passwordOtp = OTP
+        await matchingUser.save()
+    
+        const message = {
+            to: email,
+            from: 'honeycoupleart@gmail.com',
+            subject: 'Password Reset Request',
+            text: 'Click the link below to reset your password (this link will expire in 1 hour):\n\n${resetLink}. Copy your reset OTP from here for verification. ${OTP}',
+            html: `
+            <p>Click the link below to reset your password (this link will expire in 1 hour):</p>
+            <a href="${resetLink}">Reset Password</a>
+            <p>Copy your reset OTP from here for verification.</p>
+            <h6>${OTP}</h6>`
+        }
+    
+        await sgMail.send(message)
+        return res.status(200).json({ 
+            message: 'Password reset email sent!',
+            data: message})
+    } catch (error) {
+        console.error('Error sending email:', error.res ? error.res.body : error.message)
+        return res.status(500).json({
+            message: 'Failed to send reset email. Please try again later.',
+            error: error.res ? error.res.body : error.message,
+        })
+    }
+}
 
 module.exports = { userLogin, userRegistration, forgotPassword, updateUser, updatePassword }
